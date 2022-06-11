eb1e3d6b3570282e1876f2b67c20c829d08899ff
pejosecalderont <cloudencuestasinternas2021@hotmail.com>
2022-06-11 10:44:36 -0500

    integration 1 test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+    import axios from "axios"
+    import { onMount } from "svelte"
+    let resu =""
+    let error = null
+    onMount(async () => {
+        try {
+            const res = await axios.get(`/api/HttpExample?name=jose`)
+            console.log('res', res)
+            resu = res.data
+        } catch (e) {
+            error = e;
+        }
+    });
+</script>
+<h1>Bienvenido to SvelteKit {resu}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
