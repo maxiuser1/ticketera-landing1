e8d422d8c0d0c9ab47173bddf8d91b5f46629a81
pejosecalderont <cloudencuestasinternas2021@hotmail.com>
2022-06-12 22:55:34 -0500

    implement automatic carousel

--- src/components/Carousel/Carousel.svelte
+++ src/components/Carousel/Carousel.svelte
@@ -1,38 +1,120 @@
-<section aria-label="carousel">
-	<div class="carousel">
-		<div class="slide current" style="background-image: url('https://source.unsplash.com/78A265wPiO4')" />
-		<div class="slide " style="background-image: url('https://source.unsplash.com/eOpewngf68w')" />
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let items = ['a', 'b', 'c'];
+	let cantidad = 3;
+	let ancho = 100 * cantidad;
+	let mini = 100 / cantidad;
+	let translate = 0;
+	let _interval: any;
+		let iteraciones = 0;
+
+	const handleClick = (idx: number) => {
+		console.log('llego ', idx);
+		translate = idx * (mini * -1);
+	};
+
+	onMount(() => {
+		start();
+	});
+
+	const start = () => {
+		let current = 0;
+		return new Promise((resolve) => {
+		   _interval = setIntervalImmediate(() => {
+				handleClick(current);
+				current++;
+				iteraciones++;
+				if (current == items.length) {
+					current = 0;
+				}
+				if(iteraciones > 10){
+					stopInterval();
+				}
+			}, 1000);
+		});
+	};
+
+	const stopInterval = () => clearInterval(_interval);
+
+	const setIntervalImmediate = (fn: Function, ms: number) => {
+		fn();
+		return setInterval(fn, ms);
+	};
+</script>
+
+<section class="carousel" aria-label="carousel">
+	<div class="grande" style:width="{ancho}%" style:transform="translateX({translate}%)">
 		<div
-			class="slide "
-			style="background-image: url('https://source.unsplash.com/ndN00KmbJ1c')"
-		/>
+			class="slide"
+			style="background-image: url('https://source.unsplash.com/78A265wPiO4');width: {mini}%;"
+		>
+			img1
+		</div>
+		<div
+			class="slide"
+			style="background-image: url('https://source.unsplash.com/eOpewngf68w');width: {mini}%;"
+		>
+			img2
+		</div>
+		<div
+			class="slide"
+			style="background-image: url('https://source.unsplash.com/ndN00KmbJ1c');width: {mini}%;"
+		>
+			img3
+		</div>
+	</div>
+	<div class="botonera">
+		<ul class="puntos">
+			{#each items as item, idx}
+				<li class="punto" on:click={() => handleClick(idx)} />
+			{/each}
+		</ul>
 	</div>
 </section>
 
 <style lang="scss">
 	.carousel {
-		display: flex;
-		flex-flow: row;
-		margin: 0 auto;
-        height: 500px;
-		max-width: 100%;
-		position: relative;
-	}
-
-	.slide {
-		background-repeat: no-repeat;
-		background-size: cover;
-		display: none;
-		flex: 0 0 100%;
-		opacity: 0;
-		position: relative;
-		vertical-align: top;
 		width: 100%;
+		overflow: hidden;
+		position: relative;
 
-		&.current {
-			display: inline-block;
-			
-			opacity: 1;
+		.grande {
+			display: flex;
+			flex-flow: row nowrap;
+			justify-content: flex-start;
+			align-items: center;
+			transition: all 0.5s ease;
+		}
+
+		.slide {
+			color: yellow;
+			font-size: 20px;
+			background-repeat: no-repeat;
+			background-size: cover;
+			height: 400px;
+		}
+
+		.botonera {
+			width: 100%;
+			position: absolute;
+			bottom: 0px;
+			display: flex;
+			justify-content: center;
+		}
+
+		.puntos {
+			display: flex;
+			flex-flow: row nowrap;
+		}
+
+		.punto {
+			width: 1em;
+			cursor: pointer;
+			height: 1em;
+			background-color: blue;
+			margin: 1em;
+			border-radius: 50%;
 		}
 	}
 </style>
