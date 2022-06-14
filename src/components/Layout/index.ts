b8a713bd31750398cb1d3b2bf8e11cdf1cd5fc59
pejosecalderont <cloudencuestasinternas2021@hotmail.com>
2022-06-14 01:13:47 -0500

    fix store local

--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -2,4 +2,3 @@ export { default as SearchBox } from './SearchBox';
 export { default as Sections } from './Sections';
 export { default as Cards } from './Cards';
 export { default as Carousel } from './Carousel';
-export { events, getEvents } from './store';
