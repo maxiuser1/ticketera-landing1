8fa0b0583b5f75cd0e0ab1ea3f685aa8cf5aa258
pejosecalderont <cloudencuestasinternas2021@hotmail.com>
2022-06-15 20:24:28 -0500

    implement slug

---
+++ src/routes/[slug].svelte
@@ -0,0 +1,20 @@
+<script lang="ts" context="module">
+import type { Load } from '@sveltejs/kit';
+  type Params = { slug: string };
+
+  export const load: Load<Params> = async ({ params }) => {
+    const slug = params.slug;
+    return {
+      props: {
+        slug
+      }
+    };
+  };
+</script>
+
+<script lang="ts">
+    
+    export let slug: string;
+  </script>
+
+  <h1>hola {slug}</h1>
